ce71868b461acbf2e7ed28a6701bc4c89b1d3e6a
arsal381 <111340503+arsal381@users.noreply.github.com>
2024-01-27 21:57:03 +0500

    Excel Download V1.0

--- app/Jobs/CreateExcel.php
+++ app/Jobs/CreateExcel.php
@@ -35,7 +35,7 @@ class CreateExcel implements ShouldQueue
         $data = $data->toJson();
         Storage::disk('public')->put('temp_file.txt', $data);
         $path = Storage::disk('local')->path('public\test.py');
-
+        dd($path);
         exec("python3 {$path}", $output, $output);
         dd($output,$output);
         $user = auth()->user();
