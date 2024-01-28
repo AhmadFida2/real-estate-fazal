d7b1ecbb4d1b3834b113815ab8bc3397361a8f2c
arsal381 <111340503+arsal381@users.noreply.github.com>
2024-01-28 22:04:31 +0500

    Excel Download V1.0

--- app/Filament/Resources/AssignmentResource/Pages/CreateAssignment.php
+++ app/Filament/Resources/AssignmentResource/Pages/CreateAssignment.php
@@ -4,10 +4,25 @@ namespace App\Filament\Resources\AssignmentResource\Pages;
 
 use App\Filament\Resources\AssignmentResource;
 use Filament\Actions;
+use Filament\Notifications\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateAssignment extends CreateRecord
 {
     protected static string $resource = AssignmentResource::class;
+
+    protected function afterCreate($record): void
+    {
+        $user = $record->user();
+        Notification::make()
+            ->title('New Assignment!')
+            ->info()
+            ->body('You have a new assignment.')
+            ->actions([
+                Action::make('view')
+                ->url(AssignmentResource::getUrl())
+            ])->sendToDatabase($user);
+    }
 
 }
