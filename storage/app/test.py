3c378b87dc812778e1b39b637e5dab07a682e18a
arsal381 <111340503+arsal381@users.noreply.github.com>
2024-02-01 20:42:28 +0500

    Excel Download V1.0

--- storage/app/test.py
+++ storage/app/test.py
@@ -7,7 +7,6 @@ from openpyxl import load_workbook
 from openpyxl.drawing.image import Image as OpenpyxlImage
 from openpyxl.utils import get_column_letter
 
-os.chdir("/home/forge/arsal.click/public")
 temp_image_dir = 'storage/temp_images'
 os.makedirs(temp_image_dir, exist_ok=True)
 
