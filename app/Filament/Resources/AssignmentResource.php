576fc8d24c7de4b26869c08016bac57946f95c8f
arsal381 <111340503+arsal381@users.noreply.github.com>
2024-02-03 20:03:01 +0500

    Release Version 1.0

--- app/Filament/Resources/AssignmentResource.php
+++ app/Filament/Resources/AssignmentResource.php
@@ -92,7 +92,7 @@ class AssignmentResource extends Resource
                             Grid::make(4)
                                 ->schema([
                                     TextEntry::make('payment_info.payment_amount')->label('Payment Amount')->money('USD'),
-                                    TextEntry::make('payment_info.payment_date')->label('Payment Date')->dateTime(),
+                                    TextEntry::make('payment_info.payment_date')->label('Payment Date')->dateTime('d M Y'),
                                     TextEntry::make('payment_info.invoice_date')->label('Invoice Date')->dateTime(),
                                     TextEntry::make('payment_info.invoice_amount')->label('Invoice Amount')->money('USD'),
                                 ])
