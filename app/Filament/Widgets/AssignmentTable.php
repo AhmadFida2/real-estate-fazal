7525276451b976c71ec54756d2c6e544af41eea4
arsal381 <111340503+arsal381@users.noreply.github.com>
2024-02-03 13:37:28 +0500

    Excel Download V1.0

---
+++ app/Filament/Widgets/AssignmentTable.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Assignment;
+use Carbon\Carbon;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class AssignmentTable extends BaseWidget
+{
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Assignment::query()->latest()->take(5))
+            ->columns([
+                Tables\Columns\TextColumn::make('client'),
+                Tables\Columns\TextColumn::make('start_date'),
+                Tables\Columns\TextColumn::make('due_date')
+                ->formatStateUsing(fn($state) => Carbon::parse($state)->diffForHumans()),
+                Tables\Columns\TextColumn::make('property_name'),
+                Tables\Columns\TextColumn::make('loan_number'),
+                Tables\Columns\TextColumn::make('city'),
+                Tables\Columns\TextColumn::make('state'),
+                Tables\Columns\TextColumn::make('zip'),
+            ]);
+
+    }
+
+    public static function canView(): bool
+    {
+        return !auth()->user()->is_admin;
+    }
+}
