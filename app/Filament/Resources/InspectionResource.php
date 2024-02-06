4389efc8cbaa1ca249786b1156957d5c3b7b59e5
arsal381 <111340503+arsal381@users.noreply.github.com>
2024-02-06 15:14:42 +0500

    Release Version 1.0

--- app/Filament/Resources/InspectionResource.php
+++ app/Filament/Resources/InspectionResource.php
@@ -692,7 +692,7 @@ class InspectionResource extends Resource
                         Section::make('Deffered Maintenance Items')
                             ->schema([
                                 Repeater::make('deferred_items')
-                                    ->grid(3)
+                                    ->columns(3)
                                     ->schema([
                                         TextInput::make('description')->label('Description')
                                             ->helperText('Identify Item and Describe Condition (including location)'),
