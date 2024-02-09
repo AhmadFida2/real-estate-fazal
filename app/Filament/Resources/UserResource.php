60b8fa8f52e3b77dcb576a5418da497828fa0988
arsal381 <111340503+arsal381@users.noreply.github.com>
2024-02-09 21:03:56 +0500

    Release Version 1.0

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -45,7 +45,7 @@ class UserResource extends Resource
                             Forms\Components\Toggle::make('is_active')->label('Active')
                                 ->required(),
                         ])->grow(false)
-                ]),
+                ])->columnSpanFull(),
 
 
             ]);
