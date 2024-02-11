fb22b75ac94e204d422736cb7b01511543f5ca8e
arsal381 <111340503+arsal381@users.noreply.github.com>
2024-02-11 21:24:26 +0500

    Release Version 1.0

--- resources/views/panel-footer.blade.php
+++ resources/views/panel-footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="fixed bottom-0 left-0 z-10 w-full p-2 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+<footer class="fixed bottom-0 left-0 z-10 w-full p-1 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
     <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024
         <a href="#" class="hover:underline">MBA IMS™</a>. All Rights Reserved.
     </span>
