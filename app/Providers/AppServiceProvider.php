d85e3ba2fbe343398cf246910d6a677f1f80750a
arsal381 <111340503+arsal381@users.noreply.github.com>
2024-02-11 21:22:50 +0500

    Release Version 1.0

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Filament\Support\Facades\FilamentView;
+use Filament\View\PanelsRenderHook;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Support\ServiceProvider;
@@ -24,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     {
         JsonResource::withoutWrapping();
         ResourceCollection::withoutWrapping();
+        FilamentView::registerRenderHook(PanelsRenderHook::BODY_END, fn() : View => view('panel-footer'));
     }
 }
