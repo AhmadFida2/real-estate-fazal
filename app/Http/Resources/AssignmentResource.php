ddbc617d762edc77bdcbed486fa255398bcc1d2d
arsal381 <111340503+arsal381@users.noreply.github.com>
2024-02-20 21:15:35 +0500

    Release Version 1.0

--- app/Http/Resources/AssignmentResource.php
+++ app/Http/Resources/AssignmentResource.php
@@ -36,7 +36,7 @@ class AssignmentResource extends JsonResource
             'state' => $this->state,
             'zip' => $this->zip,
             'invoice_info' => $this->payment_info,
-            'payments' => $this->payments()
+            'payments' => $this->payments()->get()
         ];
     }
 }
